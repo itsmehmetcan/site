13d6c164d7a39e60b1859adcbb6d3ebff5dd0568
Mehmetcan Kaygusuz <68811918+mehmetcankaygusuz@users.noreply.github.com>
2022-08-02 19:40:33 +0300

    Update repos.jsx

--- pages/api/repos.jsx
+++ pages/api/repos.jsx
@@ -1,6 +1,6 @@
 const fetch = require('isomorphic-unfetch')
 
-let key1 = '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';
+let key1 = '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';
 let key2 = '7kIiTmNtBqj2sOC0S0zxD';
 
 export default async (req, res) => {
